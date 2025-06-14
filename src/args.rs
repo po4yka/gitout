b41054423e6cc1c402768283a060e38d41a9bcf4
Nikita Pochaev <pochaev.nik@gmail.com>
2025-06-14 16:05:25 +0400

    Fix formatting

--- src/args.rs
+++ src/args.rs
@@ -4,44 +4,44 @@ use clap::Parser;
 
 #[derive(Debug, PartialEq, Parser)]
 pub struct Args {
-        /// Configuration file
-        pub config: PathBuf,
+	/// Configuration file
+	pub config: PathBuf,
 
 	/// Backup directory
-        pub destination: PathBuf,
+	pub destination: PathBuf,
 
 	/// Enable verbose logging
-        #[arg(short, long)]
-        pub verbose: bool,
+	#[arg(short, long)]
+	pub verbose: bool,
 
 	/// Enable experimental repository archiving
-        #[arg(long)]
-        pub experimental_archive: bool,
+	#[arg(long)]
+	pub experimental_archive: bool,
 
 	/// Print actions instead of performing them
-        #[arg(long)]
-        pub dry_run: bool,
+	#[arg(long)]
+	pub dry_run: bool,
 }
 
 pub fn parse_args() -> Args {
-        Args::parse()
+	Args::parse()
 }
 
 #[cfg(test)]
 mod tests {
-        use super::*;
-        use clap::Parser;
+	use super::*;
+	use clap::Parser;
 
 	#[test]
 	fn from_iter_parses_all_flags() {
-                let args = Args::parse_from([
-                        "gitout",
-                        "config.toml",
-                        "dest",
-                        "-v",
-                        "--experimental-archive",
-                        "--dry-run",
-                ]);
+		let args = Args::parse_from([
+			"gitout",
+			"config.toml",
+			"dest",
+			"-v",
+			"--experimental-archive",
+			"--dry-run",
+		]);
 
 		assert_eq!(args.config, PathBuf::from("config.toml"));
 		assert_eq!(args.destination, PathBuf::from("dest"));
